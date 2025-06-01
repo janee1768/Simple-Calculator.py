85aaf001b62cee4005d67e741547818fb1ae5d77
Mashal Arshad <mashalarshad207@gmail.com>
2025-06-01 21:12:33 +0500

    Add files via upload

---
+++ simple calculator.py
@@ -0,0 +1,16 @@
+#Simple Calculator
+Num_1 = int(input("Enter first number: "))
+Num_2 = int(input("Enter second number: "))
+operation = input("Enter operation(+,-,*,/): ")
+if operation == "+":
+    print(Num_1 + Num_2)
+elif operation == "-":
+    print(Num_1 - Num_2)
+elif operation == "*":
+    print(Num_1*Num_2)
+elif operation == "/":
+    print(Num_1/Num_2)
+else:
+    print("Invalid operation")
+# This code is a simple calculator that performs basic arithmetic operations.
+
