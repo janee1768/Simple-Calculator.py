a1897482bf82629259550f5dcf4a6a0dcda76011
Mashal Arshad <mashalarshad207@gmail.com>
2025-08-22 15:33:28 +0500

    Update simple calculator.py

--- simple calculator.py
+++ simple calculator.py
@@ -1,19 +1,57 @@
 #Simple Calculator
 
-Num_1 = int(input("Enter first number: "))
-Num_2 = int(input("Enter second number: "))
-operation = input("Enter operation(+,-,*,/): ")
-if operation == "+":
-    print(Num_1 + Num_2)
-elif operation == "-":
-    print(Num_1 - Num_2)
-elif operation == "*":
-    print(Num_1*Num_2)
-elif operation == "/":
-    print(Num_1/Num_2)
-else:
-    print("Invalid operation")
+Num_1 = int(input("Enter number: "))
+
+operation = input("Enter operation(+,-,*,/,^2): ")
+
+while True:
+    #Taking square of the number
+
+  if operation == "^2":
+    Num_1 = Num_1 ** 2 
+    print("Answer: ",Num_1)
+    break
+
+  if operation in ['+','-','*','/']:
+   Num_2 = int(input("Enter 2nd number: "))
+
+
+    # Taking sum of the 2 numbers 
+
+  elif operation == "+":
+        Num_1 += Num_2 
+        print("Answer: ",Num_1)
+        break
+
+    # Taking the difference between  2 numbers
+
+  elif operation == "-":
+        Num_1 -= Num_2 
+        print("Answer: ",Num_1)
+        break 
+
+    # Multiplying both numbers 
+
+  elif operation == "*":
+        Num_1 *= Num_2 
+        print("Answer: ",Num_1)
+        break 
+
     
-# This code is a simple calculator that performs basic arithmetic operations.
+    # Dividing both numbers 
 
+  elif operation == "/":
+      try:
+          Num_1 /= Num_2 
+          print("Answer: ",Num_1)
 
+      except ZeroDivisionError :
+          print("Dividing by zero is not allowed!.")
+
+      break 
+  else: 
+      print(" Invalid operation use following (+,-,*,/,^2)")
+      break 
+        
+#End 
+    
